585cb63f0bfc8f5c6e36e466d18eca7129b4e997
Martin Kilbinger <martin.kilbinger@cea.fr>
2019-08-20 19:18:10 +0200

    __init__ modif

--- shear_bias/__init__.py
+++ shear_bias/__init__.py
@@ -11,10 +11,11 @@ from __future__ import print_function
 from . import *
 from .info import __version__, __whoami__
 
-from data import *
-from errors import *
-from misc import *
-from plot import *
-from shapes import *
-from simulations import *
+from .shear_bias import *
+#from data import *
+#from errors import *
+#from misc import *
+#from plot import *
+#from shapes import *
+#from simulations import *
 
